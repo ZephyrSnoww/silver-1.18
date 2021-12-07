82f09c118ff16c615e3e882051060a762157ea47
Sylas <38060892+glacierheavner@users.noreply.github.com>
2021-12-07 12:51:10 -0700

    bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb

--- src/main/java/ag/ag/blocks/Generator.java
+++ src/main/java/ag/ag/blocks/Generator.java
@@ -5,19 +5,24 @@ import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.ScreenHandler;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
 public class Generator extends BlockWithEntity implements BlockEntityProvider {
     public Generator(Settings settings) {
         super(settings);
+        setDefaultState(this.getStateManager().getDefaultState().with(Properties.HORIZONTAL_FACING, Direction.NORTH));
+//        WHAT ZE FUCK
     }
 
     @Override
@@ -80,5 +85,10 @@ public class Generator extends BlockWithEntity implements BlockEntityProvider {
     @Override
     public int getComparatorOutput(BlockState state, World world, BlockPos pos) {
         return ScreenHandler.calculateComparatorOutput(world.getBlockEntity(pos));
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return (BlockState) this.getDefaultState().with(Properties.HORIZONTAL_FACING, ctx.getPlayerFacing().getOpposite());
     }
 }
